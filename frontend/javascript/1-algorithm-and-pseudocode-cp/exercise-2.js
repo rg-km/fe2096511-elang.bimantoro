28ba3304dac408b8588e69822123ee4d2b04720a
elangb <duasanga@gmail.com>
2022-03-22 20:28:31 +0700

    push exercise-2.js

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
@@ -2,3 +2,23 @@
 // Seseorang diperbolehkan membuat SIM jika usianya lebih besar atau sama dengan 17 tahun.
 
 // TODO: answer here
+// ** CODES
+// const prompt = require("prompt-sync")();
+// const umur = parseInt(prompt("Masukkan umur : "));
+
+// if (umur >= 17) {
+//     console.log("umur " + umur + " tahun boleh membuat")
+// } else {
+//     console.log("umur " + umur + " tahun tidak boleh membuat");
+// }
+
+
+// START
+// TAMPIL "Masukkan Umur : "
+// DEKLARASI umur
+// IF umur >= 17 THEN
+// 	TAMPIL "Boleh  atau Dapat Membuat SIM"
+// ELSE
+// 	TAMPIL "Tidak Boleh atau Dapat Membuat SIM"
+// END IF
+// STOP
