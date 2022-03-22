abb0aedef9c72dc3bb2251536e911f852983fc8a
elangb <duasanga@gmail.com>
2022-03-22 20:38:13 +0700

    push script-2.js

--- frontend/javascript/3-comparison-cp/script-2.js
+++ frontend/javascript/3-comparison-cp/script-2.js
@@ -1,4 +1,5 @@
 // Masukan 3 bilangan bulat menggunakan if condition
+const prompt = require("prompt-sync")();
 const num1 = parseInt(prompt("Masukan bilangan pertama: "));
 const num2 = parseInt(prompt("Masukan bilangan kedua: "));
 const num3 = parseInt(prompt("Masukan bilangan ketiga: "));
@@ -6,6 +7,13 @@ const num3 = parseInt(prompt("Masukan bilangan ketiga: "));
 let largest;
 
 // TODO: answer here
+if (num1>num2 && num1>num3) {
+    console.log(largest=num1);
+} else if (num2>num1 && num2>num3) {
+    console.log(largest=num2);
+} else if (num3>num1 && num3>num2) {
+    console.log(largest=num3);
+}
 
 // Menampilkan hasil
 console.log("Bilangan terbesar adalah " + largest);
