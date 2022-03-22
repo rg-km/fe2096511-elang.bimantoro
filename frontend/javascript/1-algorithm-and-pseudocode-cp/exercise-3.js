5cba8ab71d8365cc46bbacd128d082fef03f0189
elangb <duasanga@gmail.com>
2022-03-22 20:28:55 +0700

    push exercise-3.js

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -1,3 +1,26 @@
 // Buatlah pseudocode untuk menjumlahkan bilangan genap yang ada di antara 1 sampai 10
 
 // TODO: answer here
+
+
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let total = 0;
+// const genap = [];
+
+// for (let i = 0; i < angka.length; i++) {
+//     if (angka[i] % 2 == 0) {
+//         total += angka[i];
+//         genap.push(angka[i]);
+//     }
+// }
+// console.log("bilangan genap dari 1 sampai 10 adalah " + genap);
+// console.log("total bilangan genap = " + total);
+
+// START
+// FOR i=1 to 10 
+// 	IF i%2 == 0 THEN
+// 		jumlah = jumlah + i;
+// 	END IF
+// END FOR
+// DISPLAY(jumlah)
+// STOP
