bce122e27e92098ecac8a6e7bd8944ffcecd656f
elangb <duasanga@gmail.com>
2022-03-29 12:00:52 +0700

    submit ganjil-genap-cp

--- frontend/javascript/exercise/ganjil-genap-cp/main.js
+++ frontend/javascript/exercise/ganjil-genap-cp/main.js
@@ -10,31 +10,36 @@ Setiap plat nomer di pisahkan oleh karakter ";"
 
 function ganjilGenap(plat) {
   // TODO: answer here
-  switch (plat) {
-    case '':
-      return 'plat tidak ditemukan'
-    case ';':
-      return 'invalid data'
-    default:
-      let arr = plat.split(';')
-      let odd = 0
-      let even = 0
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {
-          even++
-        } else {
-          odd++
-        }
+  if (plat === undefined) {
+    return "invalid data";
+  }
+  else if (plat === "") {
+    return "plat tidak ditemukan";
+  }
+  else {
+    var platGanjil = 0;
+    var platGenap = 0;
+    var platStr = plat.split(";");
+    for (var i = 0; i < platStr.length; i++) {
+      if (platStr[i] % 2 === 0) {
+        platGenap++;
       }
-      if (odd === 0 && even === 0) {
-        return 'plat ganjil tidak ditemukan dan plat genap tidak ditemukan'
-      } else if (odd === 0) {
-        return `plat genap sebanyak ${even} dan plat ganjil tidak ditemukan`
-      } else if (even === 0) {
-        return `plat ganjil sebanyak ${odd} dan plat genap tidak ditemukan`
-      } else {
-        return `plat genap sebanyak ${even} dan plat ganjil sebanyak ${odd}`
+      else {
+        platGanjil++;
       }
+    }
+    if (platGanjil === 0 && platGenap === 0) {
+      return "plat ganjil tidak ditemukan dan plat genap tidak ditemukan";
+    }
+    else if (platGanjil === 0) {
+      return "plat genap sebanyak " + platGenap + " dan plat ganjil tidak ditemukan";
+    }
+    else if (platGenap === 0) {
+      return "plat ganjil sebanyak " + platGanjil + " dan plat genap tidak ditemukan";
+    }
+    else {
+      return "plat genap sebanyak " + platGenap + " dan plat ganjil sebanyak " + platGanjil;
+    }
   }
 }
 
@@ -42,6 +47,6 @@ console.log(ganjilGenap('2341;3429;864;1309;1276')) //plat genap sebanyak 2 dan 
 console.log(ganjilGenap('2347;3429;1305')) //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
 console.log(ganjilGenap('864;1308;1276;1432')) //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
 console.log(ganjilGenap('')) //plat tidak ditemukan
-// console.log(ganjilGenap()) //invalid data belum
+console.log(ganjilGenap()) //invalid data belum
 
 module.exports = ganjilGenap
