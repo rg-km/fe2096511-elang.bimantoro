9fc0791eee55262ac30210ae517ebeeabf741a60
elangb <duasanga@gmail.com>
2022-03-31 10:38:10 +0700

    update 7-object-cp

--- frontend/javascript/7-object-cp/script-1.js
+++ frontend/javascript/7-object-cp/script-1.js
@@ -11,11 +11,25 @@ let programming = {
 // buat kode untuk manipulasi data dalam objek berdasarkan instruksi berikut
 
 // 1. Tulis perintah untuk menambahkan bahasa "Go" ke akhir array languages.
+let langauge = programming.languages.push("Go");
 // 2. Ubah difficulty menjadi 7.
+programming.difficulty = 7;
 // 3. Menggunakan kata kunci delete, tulis perintah untuk menghapus key jokes dari objek programming.
+delete programming.jokes;
 // 4. Tulis perintah untuk menambahkan key baru yang disebut isFun dan nilai true ke objek programming.
+programming.isFun = true;
 // 5. Menggunakan loop, iterasi array languages dan console.log semua bahasa.
+programming.languages.forEach(function(lang) {
+    console.log(lang);
+});
 // 6. Menggunakan loop, console.log semua key dalam objek pemrograman.
+for (let key in programming) {
+    console.log(key);
+}
+for (let key in programming) {
+    console.log(programming[key]);
+}
 // 7. Menggunakan loop, console.log semua value dalam objek pemrograman.
 
+console.log(programming);
 // TODO: answer here
