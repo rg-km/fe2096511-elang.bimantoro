e232a3b571a6fd700a0a0a67063aabfa2f0ad610
elangb <duasanga@gmail.com>
2022-03-31 10:38:10 +0700

    update 7-object-cp

--- frontend/javascript/7-object-cp/script-2.js
+++ frontend/javascript/7-object-cp/script-2.js
@@ -4,41 +4,38 @@
 // - substract() mengurangi jumlah nilai yang disimpan.
 // - multiply() mengalikan nilai yang disimpan.
 // - division() membagi nilai yang disimpan.
-const prompt = require("prompt-sync")();
-const angka1 = parseInt(prompt("Masukkan bialngan pertama: "));
-const angka2 = parseInt(prompt("Masukkan bialngan kedua: "));
+// const prompt = require("prompt-sync")();
+// const angka1 = parseInt(prompt("Masukkan bialngan pertama: "));
+// const angka2 = parseInt(prompt("Masukkan bialngan kedua: "));
 
 let calculator = {
   // Tulis kode di sini
   // TODO: answer here
-
-  // Penjumlahan
-  sum(angka1, angka2) {
-    let hasil = angka1 + angka2;
-    return 'Hasil penjumlahan ' + angka1 + ' + ' + angka2 + ' = ' + hasil;
+  num1: 0,
+  num2: 0,
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b;
   },
-  
-  // Pengurangan
-  substract(angka1, angka2) {
-    let hasil = angka1 - angka2;
-    return 'Hasil pengurangan ' + angka1 + ' - ' + angka2 + ' = ' + hasil;
+  sum() {
+    return this.num1 + this.num2;
   },
-  
-  // Perkalian
-  multiply(angka1, angka2) {
-    let hasil = angka1 * angka2;
-    return 'Hasil perkalian ' + angka1 + ' * ' + angka2 + ' = ' + hasil;
+  substract() {
+    return this.num1 - this.num2;
   },
-  
-  // Pembagian
-  division(angka1, angka2) {
-    let hasil = angka1 / angka2;
-    return 'Hasil pembagian ' + angka1 + ' / ' + angka2 + ' = ' + hasil;
+  multiply() {
+    return this.num1 * this.num2;
+  },
+  division() {
+    return this.num1 / this.num2;
   }
-};
-  
-// calculator.read();
-console.log(calculator.sum(angka1, angka2));
-console.log(calculator.substract(angka1, angka2));
-console.log(calculator.multiply(angka1, angka2));
-console.log(calculator.division(angka1, angka2));
+  };
+
+
+const num1 = 10;
+const num2 = 5;
+calculator.read(num1, num2);
+console.log(calculator.sum());
+console.log(calculator.substract());
+console.log(calculator.multiply());
+console.log(calculator.division());
