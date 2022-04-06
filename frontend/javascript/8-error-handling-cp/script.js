034d1cc97d2d80fe6d843cff963a5fa9349bc347
elangb <duasanga@gmail.com>
2022-04-06 10:51:14 +0700

    update 8-error-handling-cp

--- frontend/javascript/8-error-handling-cp/script.js
+++ frontend/javascript/8-error-handling-cp/script.js
@@ -1,4 +1,4 @@
-// Buatlah fungsi yang mengembalikan nama bulan dari array berdasarkan nilai bilangan yang dikirim ke fungsi tersebut. 
+// Buatlah fungsi yang mengembalikan nama bulan dari array berdasarkan nilai bilangan yang dikirim ke fungsi tersebut.
 // Jika nilainya tidak sesuai dengan bulan (1-12), kembalikan error 'Masukan salah. Input bulan antara 1-12'.
 //
 // Contoh 1
@@ -10,17 +10,38 @@
 // Output: "Masukan salah. Input bulan antara 1-12"
 
 function getMonthName(monthNumber) {
-    monthNumber = monthNumber - 1;
-    let months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli',
-                  'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-    if (months[monthNumber]) {
-      console.log("Bulan ke-" + (monthNumber + 1) + " adalah " + months[monthNumber])
-    } else {
-      throw new Error("Masukan salah. Input bulan antara 1-12");
-    }
+  monthNumber = monthNumber - 1;
+  let months = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  if (months[monthNumber]) {
+    console.log(
+      "Bulan ke-" + (monthNumber + 1) + " adalah " + months[monthNumber]
+    );
+  } else {
+    throw new Error("Masukan salah. Input bulan antara 1-12");
   }
-  
-  // TODO: answer here
-    let myMonth = parseInt(prompt("Masukan bilangan: "));
-    getMonthName(myMonth);
-  // TODO: answer here
+}
+
+// TODO: answer here
+const prompt = require("prompt-sync")();
+let myMonth = parseInt(prompt("Masukan bilangan: "));
+// getMonthName(myMonth);
+// TODO: answer here
+
+try {
+  getMonthName(myMonth);
+} catch (err) {
+  console.log(err.message);
+}
