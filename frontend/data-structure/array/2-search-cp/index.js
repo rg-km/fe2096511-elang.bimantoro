cee8c8e972deeb819bb9685badce43cd9e6b493f
elangb <duasanga@gmail.com>
2022-04-21 10:05:29 +0700

    submit sort-cp

--- frontend/data-structure/array/2-search-cp/index.js
+++ frontend/data-structure/array/2-search-cp/index.js
@@ -1,5 +1,12 @@
 function searchMatch(arr1, arr2) {
     let result = [];
+    for (let i=0; i<arr1.length; i++) {
+        for (let j=0; j<arr2.length; j++) {
+            if (arr1[i] == arr2[j]) {
+                result.push(arr1[i])
+            }
+        }
+    }
     return result // TODO: replace this
 }
 
