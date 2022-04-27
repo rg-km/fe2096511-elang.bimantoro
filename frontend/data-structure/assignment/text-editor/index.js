527392549a16e9aacf75e181ec866ea80a8e5d91
elangb <duasanga@gmail.com>
2022-04-28 05:10:07 +0700

    submit text-editor

--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,53 @@
 module.exports = class TextEditor {
     constructor() {
         // TODO: answer here
+        this.stack = []
+        this.undoStack = []
+        this.redoStack = []
+        this.top = -1
     }
 
     write(c) {
         // TODO: answer here
+        this.undoStack.push(c)
+        this.top++;
+        this.stack.push(c)
+
     }
 
     read() {
         // TODO: answer here
+        let result = "";
+
+        for(let i = 0; i < this.stack.length; i++) {
+            result += this.stack[i]
+        }
+        if (result === "") {
+            return ""
+        } else {
+            return result
+        }
+        
     }
 
     undo() {
         // TODO: answer here
+        if (this.undoStack.length === 0) {
+            return ""
+        } else {
+            let c = this.undoStack.pop();
+            this.redoStack.push(c);
+            this.stack.pop();
+        }
     }
     
     redo() {
         // TODO: answer here
+        if(this.redoStack.length === 0) {
+            return ""
+        }
+        let c = this.redoStack.pop();
+        this.undoStack.push(c);
+        this.stack.push(c);
     }
 }
