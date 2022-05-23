f8fd090255508ed74b57cb71d7a16d50f25b0013
elangb <duasanga@gmail.com>
2022-05-23 09:56:18 +0700

    submit react-project

--- frontend/react/react-project/src/components/UploadForm.js
+++ frontend/react/react-project/src/components/UploadForm.js
@@ -1,5 +1,17 @@
 // TODO: answer here
+import "./App.css";
+import ".index.css";
 
 export default function UploadForm({onSubmit}) {
   // TODO: answer here
+  return (<div className="UploadForm">
+ <div className='uploadForm' aria-label='UploadForm'>
+  <form onSubmit={onSubmit} aria-label="uploadForm">
+    <input type="text" aria-label='CaptionInput' placeholder='caption'/>
+    <input type="text" aria-label='ImageInput' placeholder="ImageURL" />
+    <button type="submit" aria-label='SubmitButton' buttonOnClick = "" >Upload</button>
+  </form>
+ </div>
+ </div>
+  )
 }
