535f159a512c5f7232fbdbc96012241bb86bb960
elangb <duasanga@gmail.com>
2022-05-24 21:18:06 +0700

    update

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,22 +1,23 @@
-import "./App.css"
-import React from "react";
+import "./App.css";
 // TODO: answer here
+import React from "react";
 import Navbar from "./components/Navbar";
 import LikeDislikeButton from "./components/LikeDislikeButton";
+import PostCard from "./components/PostCard";
 
 function App() {
   // TODO: answer here
   return (
     <div aria-label="App" className="App">
-      <h1 aria-label="App Title">IG Clone</h1>
+      <h1 aria-label="App Title">Instagram Clone</h1>
 
       <div className="ImportItem">
         <Navbar />
         <LikeDislikeButton />
+        <PostCard />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
